a9a577fb6aab547eb5846623f021b1477fb8f472
amali <e.amjad2010@gmail.com>
2023-11-26 11:16:11 +0300

    q1-2

---
+++ primitives.js
@@ -0,0 +1,9 @@
+const str = "Hello, World!"; // string
+const num = 42;              // number
+const bool = true;           // boolean
+const undef = undefined;     // undefined
+
+console.log("String:", str);
+console.log("Number:", num);
+console.log("Boolean:", bool);
+console.log("Undefined:", undef);
