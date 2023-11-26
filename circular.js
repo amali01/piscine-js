80d552f254068d13b7e6f173cebb5612c6c927ff
amali <e.amjad2010@gmail.com>
2023-11-26 17:35:42 +0300

    q-11

--- circular.js
+++ circular.js
@@ -1,3 +1,5 @@
-const circular = {
-    circular: circular ,
-}
+// Create an empty object
+const circular = {};
+
+// Add a property named "circular" with a reference to itself
+circular.circular = circular;
