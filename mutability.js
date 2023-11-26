843cd2cb812a079f2c880b7ff76725d3ab1c7155
amali <e.amjad2010@gmail.com>
2023-11-26 12:39:27 +0300

    q1-4

---
+++ mutability.js
@@ -0,0 +1,8 @@
+// Shallow copy using Object.assign
+const clone1 = Object.assign({}, person);
+const clone2 = Object.assign({}, person);
+
+const samePerson = person;
+
+person.age = 78;
+person.country= 'FR';
