7ea4946d7a7abadd1bcf18db415ddcf34d3428fb
Amgad  Ali <amali@noreply.learn.reboot01.com>
2023-11-26 09:46:15 +0000

    Update 'mutability.js'

--- mutability.js
+++ mutability.js
@@ -1,3 +1,7 @@
+// // Deep copy using JSON.parse and JSON.stringify
+// const clone1 = JSON.parse(JSON.stringify(person));
+// const clone2 = JSON.parse(JSON.stringify(person));
+
 // Shallow copy using Object.assign
 const clone1 = Object.assign({}, person);
 const clone2 = Object.assign({}, person);
