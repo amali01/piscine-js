078bc9c2d9b034f1f09e50d7aae38781b719e330
amali <e.amjad2010@gmail.com>
2023-11-26 11:44:38 +0300

    q1-3

---
+++ declarations.js
@@ -0,0 +1,19 @@
+const escapeStr = "\`\\ \/ \"\'\`"; // special characters string 
+const arr = [4,'2'] ; // array 
+const obj = {
+    str : 'hi',
+    num : 1 ,
+    bool : true ,
+    undef : undefined,    
+};
+
+const nested  = {
+    arr:[4,undefined,'2'],
+    obj:{
+        str: "reboot",
+        num : 2,
+        bool : false,
+    }
+};
+
+Object.freeze(nested);
