1e445f21d9ad06f1fa1ea65f09abd3456ecec04e
amali <e.amjad2010@gmail.com>
2023-11-26 12:55:49 +0300

    q-5

---
+++ more-or-less.js
@@ -0,0 +1,19 @@
+// Function to add 1 to a number
+function more(num){
+    return num +1;
+}
+
+// Function to subtract 1 from a number
+function more(num){
+    return num -1;
+}
+
+// Function to add two numbers
+function more(num1 ,num2){
+    return num1 +num2;
+}
+
+// Function to subtract the second number from the first
+function more(num1 ,num2){
+    return num1 -num2;
+}
