db682ab94f1f7d12ac4e7ee94168fe551f58121f
amali <e.amjad2010@gmail.com>
2023-11-26 19:12:42 +0300

    q1-17

--- is.js
+++ is.js
@@ -1,14 +1,11 @@
-const is = {};
-is.obj = {};
-
 is.num = (n) => (typeof n === 'number');
 is.nan = (n) => Number.isNaN(n);
 is.str = (s) => (typeof s === 'string');
 is.bool = (b) => (typeof b === 'boolean');
 is.undef = (u) => (typeof u === 'undefined');
-is.def = (d) => !(typeof d === 'undefined');
+is.def = (d) => !is.undef(d);
 is.arr = (a) => (Array.isArray(a));
 is.obj = (o) => (typeof o === 'object' && o !== null && !Array.isArray(o));
 is.fun = (f) => (typeof f === 'function');
-is.truthy = (t) => (t == true);
-is.falsy = (f) => (f == false);
+is.truthy = (t) => (t == true ? true : false);
+is.falsy = (f) => (f == false ? true : false);
