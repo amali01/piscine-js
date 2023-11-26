0933e6110f0b56af3b368241d303ed862543d7bd
amali <e.amjad2010@gmail.com>
2023-11-26 12:56:23 +0300

    q-5

--- more-or-less.js
+++ more-or-less.js
@@ -4,16 +4,16 @@ function more(num){
 }
 
 // Function to subtract 1 from a number
-function more(num){
+function less(num){
     return num -1;
 }
 
 // Function to add two numbers
-function more(num1 ,num2){
+function add(num1 ,num2){
     return num1 +num2;
 }
 
 // Function to subtract the second number from the first
-function more(num1 ,num2){
+function sub(num1 ,num2){
     return num1 -num2;
 }
