6a33d6b4ba169658bb4b2705d409569c87d82585
amali <e.amjad2010@gmail.com>
2023-11-26 15:48:30 +0300

    q-10

---
+++ dog-years.js
@@ -0,0 +1,20 @@
+// Function to calculate a dog's age in dog years on a given planet
+function dogYears(name , DogAge){
+ // Calculate the dog's age in Earth years
+ // 31,557,600 seconds in an Earth year
+const DogYears = (DogAge / 31,557,600 ) * 7
+
+    // Orbital periods relative to Earth for various planets
+const planet = {
+    earth: 1.0,
+    mercury: 0.2408467,
+    venus: 0.61519726,
+    mars: 1.8808158,
+    jupiter: 11.862615,
+    saturn: 29.447498,
+    uranus: 84.016846,
+    neptune: 164.79132,
+}
+    // Return the calculated age on the given planet in dog years
+return planet[name] * DogYears;
+}
