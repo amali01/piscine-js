c726fa3fe01d13ad53af41d0d8fde29e2d5c66b9
amali <e.amjad2010@gmail.com>
2023-11-26 13:32:04 +0300

    q-7

--- last-first-kiss.js
+++ last-first-kiss.js
@@ -1,11 +1,13 @@
+//returns its first element or character.
 function first(arg) {
     return arg[0];
 }
-
+// returns its last element or character.
 function last(arg) {
     return arg[arg.length - 1];
 }
 
+//returned array should contain the last and first elements or characters, in that order.
 function kiss(arg) {
     return [last(arg),first(arg)];
 }
