f18c4c247ec7c6a94decc5b03cd1db0eb3738c01
amali <e.amjad2010@gmail.com>
2023-11-27 12:34:27 +0300

    q1-20

---
+++ block-chain.js
@@ -0,0 +1,21 @@
+function blockChain(data,prev){
+    if (prev === null){
+        prev = {
+            index: 0,
+            hash: '0',
+        };
+    };
+
+    const block ={
+        index: prev.index + 1,
+        hash: hashCode(
+            (prev.index+1).toString()+ prev.hash + JSON.stringify(data)
+        ),
+        data : data,
+        prev : prev,
+        chain: function (data){
+            return blockChain(data,block);
+        },
+    };
+
+}
