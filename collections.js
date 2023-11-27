e5ddb4c57012540d0e7e252045e5ef49d0540958
amali <e.amjad2010@gmail.com>
2023-11-27 11:23:31 +0300

    q1-19

---
+++ collections.js
@@ -0,0 +1,75 @@
+// from Array to Set
+function arrToSet(arr){
+    return new Set(arr);
+}
+
+// from Array to string
+function arrToStr(arr){
+    return arr.Join(", ");
+}
+
+// from Set to Array
+function setToArr(set){
+    return Array(set);
+}
+
+// from Set to string
+function setToStr(set){
+    return set.Join(", ");
+}
+
+// from string to Array
+function strToArr(str){
+    return str.split("");
+}
+
+// from string to Set
+function strToSet(str){
+    return new Set(str);
+}
+
+// from Map to Object
+function mapToObj(map){
+    const obj ={};
+    map.forEach((value,key) => {
+        obj[key]=value;
+    });
+    return obj;
+}
+
+// from Object to Array
+function objToArr(obj) {
+    return Object.values(obj);
+}
+
+// from Object to Map
+function objToMap(obj) {
+    return new Map(Object.entries(obj));
+}
+
+// from Array to Object
+function arrToObj(arr) {
+    const obj = {};
+    arr.forEach((item, index) => {
+        obj[index] = item;
+    });
+    return obj;
+}
+
+// from string to Object
+function strToObj(str) {
+    return Object.fromEntries(str.split('').map((char, index) => [index, char]));
+}
+
+// to determine the "super type" of a value
+function superTypeOf(value) {
+    if (Array.isArray(value)) {
+        return 'Array';
+    } else if (value instanceof Set) {
+        return 'Set';
+    } else if (value instanceof Map) {
+        return 'Map';
+    } else {
+        return typeof value;
+    }
+}
