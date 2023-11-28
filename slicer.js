bce3371ecce88d7a8c24b32cdc4db7d7d0271004
amali <e.amjad2010@gmail.com>
2023-11-28 17:40:50 +0300

    q2-4

--- slicer.js
+++ slicer.js
@@ -1,15 +1,15 @@
-function slice(input, index1, index2 = input.length - 1) {
+function slice(input, index1, index2 = input.length ) {
     let res = '';
     if (index1 < 0){
-        index1 = input.length + index1 
+        index1 = input.length + index1
     }
     if (typeof input === 'string') {
-      for (let i = index1; i <= index2; i++) {
+      for (let i = index1; i < index2; i++) {
         res += input[i];
       }
     } else if (Array.isArray(input)) {
        let str = input.toString().join('')
-      for (let i = index1; i <= index2; i++) {
+      for (let i = index1; i < index2; i++) {
         res += str[i];
       }
     }
