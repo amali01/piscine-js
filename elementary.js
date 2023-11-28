25d768e6968c7d60ce9c22109ad7f3508740cb58
amali <e.amjad2010@gmail.com>
2023-11-28 11:36:34 +0300

    q2-2

--- elementary.js
+++ elementary.js
@@ -1,23 +1,27 @@
 function multiply (a, b){
     // Initialize an empty string to store the repeated int
-    let result = 0;
-
-    // Repeat the int 'b' times
-    for (let i = 0; i < Math. abs(b); i++) {
-      result += a;
-    }
-    // If 'b' is negative, make the result negative
-  if (b < 0) {
-    result = 0 - result;
+    let result ;
+    if (a === 0 || b === 0) {
+      res = 0;
+      }
+   result = multiply(a,b)
+      return result
   }
-    return result
-}
+  
 
 
 function divide (a, b){
   // Initialize an empty string to store the repeated int
-  let result = 0;
-  
+  let result ;
+    if (a === 0) {
+        res = 0;
+    } else if (b === 1) {
+        res = a;
+    } else if (b === -1) {
+        res = -a;
+    } else if (b === 0) {
+        return Infinity;
+    } 
 
   // Subtract 'b' from 'a' until 'a' becomes less than 'b'
   while (a >= Math. abs(b)) {
