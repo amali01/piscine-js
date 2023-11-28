4aa0886f6f30206624f9ef6cca60ba8022d4b921
amali <e.amjad2010@gmail.com>
2023-11-28 12:45:19 +0300

    q2-2

--- elementary.js
+++ elementary.js
@@ -37,23 +37,40 @@ function divide (a, b){
              a  = Math. abs(a) - Math. abs(b);
               result++;
           }
-          
-        // If 'b' is negative, make the result negative
+
+        // check if the result need to be negative
         if (b < 0 && A < 0)  {
             return result
         } else if (b < 0 || A < 0)  {
-              result = 0 - result;
-          }
+            result = 0 - result;
+        }
       } 
     return result
 }
 
 function modulo (a, b){
-  // Subtract 'b' from 'a' until 'a' becomes less than 'b'
-  while (a >=  Math.abs(b)) {
-    a -= Math.abs(b);
-  }
-  
-  return a
-
+    // Initialize an empty string to store the repeated int
+    const A = a;
+      if (a === 0 || b > Math. abs(a)) {
+          a = 0;
+      } else if (b === 1) {
+          a = a;
+      } else if (b === -1) {
+          a = -a;
+      } else if (b === 0) {
+          return Infinity;
+      }else{
+         // Subtract 'b' from 'a' until 'a' becomes less than 'b'
+          while (Math. abs(a) >= Math. abs(b)) {
+             a  = Math. abs(a) - Math. abs(b);
+          }
+          
+        // check if the result need to be negative
+        if (b < 0 && A < 0)  {
+            return a
+        } else if (b < 0 || A < 0)  {
+              a = 0 - a;
+          }
+      } 
+    return a
 }
