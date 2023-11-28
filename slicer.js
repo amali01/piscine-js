6fdae461244504e46e3e58ac17ec6252e47a5b2a
amali <e.amjad2010@gmail.com>
2023-11-28 17:34:51 +0300

    q2-4

--- slicer.js
+++ slicer.js
@@ -1,6 +1,8 @@
 function slice(input, index1, index2 = input.length - 1) {
     let res = '';
-  
+    if (index1 < 0){
+        index1 = (input.length - 1)-index1
+    }
     if (typeof input === 'string') {
       for (let i = index1; i <= index2; i++) {
         res += input[i];
