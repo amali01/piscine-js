2815febc8cbdac79aeb84f1858cc7ec567de76d0
amali <e.amjad2010@gmail.com>
2023-11-30 13:54:50 +0300

    q2-8

--- unbreakable.js
+++ unbreakable.js
@@ -5,11 +5,12 @@ if (s.length <= 1) {
     return s
 }else{
     for (let i = 0 ; i < s.length ; i ++ ){
-        if (s[i] === div || i === s.length -1){
+        if (s[i] === div){
             res.push(s.substring(count,i));
             count = i+1;
         }
     }
+    res.push(s.substring(count));
 }
 return res ;
 }
