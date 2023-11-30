5d4150a2b94e777ce2d155d05628df1cdcd7255c
amali <e.amjad2010@gmail.com>
2023-11-30 23:44:40 +0300

    q2-12

--- get.js
+++ get.js
@@ -1,3 +1,23 @@
-function get(src , path){
-    return src.path
-}
+function get(src, path) {
+    // Split the path into keys using dot as the separator
+    const keys = path.split('.');
+  
+    // Start with the source object
+    let current = src;
+  
+    // Iterate through keys to navigate the object
+    for (const key of keys) {
+      // Check if the current key exists in the object
+      if (current.hasOwnProperty(key)) {
+        // Move to the next level
+        current = current[key];
+      } else {
+        // If the key doesn't exist, return undefined
+        return undefined;
+      }
+    }
+  
+    // Return the final value at the specified path
+    return current;
+  }
+  
