55db2273270ee961ae9d91258157cc6c3cf3c986
amali <e.amjad2010@gmail.com>
2023-11-30 23:24:05 +0300

    q2-11

---
+++ molecules-cells.js
@@ -0,0 +1,31 @@
+function RNA(s){
+  let res = ""
+    for (let i = 0 ; i < s.length ; i++) {
+      if (s[i]==='C'){
+        res += 'G'
+      }else if (s[i]==='G'){
+        res += 'C'
+      }else if (s[i]==='A'){
+        res += 'T'
+      }else if (s[i]==='U'){
+        res += 'A'
+      }
+    }
+    return res
+}
+
+function DNA(s){
+    let res = ""
+    for (let i = 0 ; i < s.length ; i++) {
+      if (s[i]==='G'){
+        res += 'C'
+      }else if (s[i]==='C'){
+        res += 'G'
+      }else if (s[i]==='T'){
+        res += 'A'
+      }else if (s[i]==='A'){
+        res += 'U'
+      }
+    }
+    return res
+}
