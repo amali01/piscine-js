fba76d01d284bc09288efa30684e3219dce4155f
amali <e.amjad2010@gmail.com>
2023-12-01 09:29:29 +0300

    q2-14

--- pyramid.js
+++ pyramid.js
@@ -5,7 +5,7 @@ function pyramid(str, num) {
       for (let k = 0; k < num - i - 1; k++) {
         res += " ";
       }
-      // Add asterisks for the pyramid
+      // Add str for the pyramid
       for (let j = 0; j < 2 * i + 1; j++) {
         res += str;
       }
