5a4b8c36fa9b7c55adafad7dbe59d5307c7a4b7b
amali <e.amjad2010@gmail.com>
2023-12-01 08:38:50 +0300

    q2-13

---
+++ triangle.js
@@ -0,0 +1,11 @@
+function triangle(str , num){
+    let res = ""
+for (let i = 0 ; i < num ; i++){
+    for (let j = 0 ; j <= i ; j++){
+        res += str 
+    }
+    res += "\n" 
+
+}
+return res
+}
